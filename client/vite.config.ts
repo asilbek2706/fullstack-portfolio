278f8be47a2d8c794cd8e6450441b011687c5c25
Asilbek <asilbekkaromatov2@gmail.com>
2026-06-21 18:11:14 +0500

    config: setup vite configuration, tsconfig path aliases and sass

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react' 
+import path from 'path'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      // @src deb yozsak shundoq portfolio papkasiga boradi
+      '@src': path.resolve(__dirname, './src'),
+      // @admin deb yozsak shundoq admin panel papkasiga boradi
+      '@admin': path.resolve(__dirname, './admin'),
+    },
+  },
 })
