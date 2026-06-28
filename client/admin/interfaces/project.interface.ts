451afca7f63b8231987d7963ae67ad4e9c00f2a2
Asilbek <asilbekkaromatov2@gmail.com>
2026-06-28 22:53:50 +0500

    feat(admin): add project TypeScript interfaces

--- client/admin/interfaces/project.interface.ts
+++ client/admin/interfaces/project.interface.ts
@@ -1,9 +1,27 @@
-export interface ProjectData {
+export interface Project {
   _id: string;
   title: string;
   description: string;
   image: string;
   technologies: string[];
   githubLink: string;
-  demoLink: string;
+  demoLink?: string;
+  createdBy: string;
+  createdAt: string;
+  updatedAt: string;
+}
+
+export interface ProjectFormData {
+  title: string;
+  description: string;
+  technologies: string[];
+  githubLink: string;
+  demoLink?: string;
+  image?: File | null;
+}
+
+export interface ApiResponse<T> {
+  success: boolean;
+  message: string;
+  data: T;
 }
